c1def8788929960b89bc541fa934c4f0d9fe11a4
Darshan S <darshans2727@gmail.com>
2025-06-24 22:48:34 +0530

    memoized list item

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,26 +1,33 @@
 import { ThemeToggleButton } from "@/features/theme";
-import { useEffect, useRef, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 
 const Home = () => {
   const [listItems, setListItems] = useState<string[]>([]);
   const listRef = useRef<HTMLDivElement>(null);
+  const [pauseAddItems, setPauseAddItems] = useState(true);
+
+  function addItems() {
+    setListItems((prev) => [...prev, "Item " + (prev.length + 1)]);
+  }
 
   useEffect(() => {
     if (listRef.current) {
       listRef.current.scrollTo({
         top: listRef.current.scrollHeight,
-        behavior: "smooth",
+        behavior: "instant",
       });
     }
   }, [listItems]);
 
   useEffect(() => {
     const intervalId = setInterval(() => {
-      setListItems((prev) => [...prev, "Item " + (prev.length + 1)]);
-    }, 500);
+      if (!pauseAddItems) {
+        addItems();
+      }
+    }, 150);
 
     return () => clearInterval(intervalId);
-  }, []);
+  }, [pauseAddItems]);
 
   return (
     <>
@@ -28,6 +35,11 @@ const Home = () => {
         <ThemeToggleButton />
       </div>
       <div className="flex flex-col gap-4 items-center justify-center min-h-screen h-full bg-white dark:bg-black colors-smooth">
+        <div className="start-stop-section">
+          <button onClick={() => setPauseAddItems(!pauseAddItems)}>
+            {pauseAddItems ? "Start" : "Stop"}
+          </button>
+        </div>
         <div ref={listRef} className="h-[500px] w-[500px] overflow-y-auto hide-scrollbar border p-2 border-black">
           {listItems.map((item, index) => (
             <ListItem key={index} item={item} />
@@ -40,7 +52,7 @@ const Home = () => {
 
 export default Home;
 
-const ListItem = ({ item }: { item: string }) => {
+const ListItem = React.memo(({ item }: { item: string }) => {
   return <div>{item}</div>;
-};
+});
 
